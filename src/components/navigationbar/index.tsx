ec7d91d2cd964b3ed379461af1eec3b289a744c7
Ian Outterside <ianoshorty@gmail.com>
2018-10-13 21:15:41 +0100

    Nav bar styling

--- src/components/navigationbar/index.tsx
+++ src/components/navigationbar/index.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react';
 import { Link } from '@reach/router';
 
-import './NavigationBar.css';
+//import './NavigationBar.css';
 
 interface Props {
   data?: any;
@@ -9,20 +9,32 @@ interface Props {
 
 const NavigationBar: React.SFC<Props> = ({ data }) => {
   return (
-    <div className="navigationBar">
-      <h1><Link to="/">Komodo</Link></h1>
-      <ul>
-        <li>
-          <Link to={'about'}>About</Link>
+    <div className="bg-black flex p-8">
+      <h1 className="flex-1">
+        <Link to="/" className="text-white no-underline uppercase">
+          Komodo
+        </Link>
+      </h1>
+      <ul className="list-reset flex-1 flex flex-row items-center justify-end">
+        <li className="w-full text-center">
+          <Link to={'about'} className="text-white no-underline">
+            About
+          </Link>
         </li>
-        <li>
-          <Link to={'case-studies'}>Case Studies</Link>
+        <li className="w-full text-center">
+          <Link to={'case-studies'} className="text-white no-underline">
+            Case Studies
+          </Link>
         </li>
-        <li>
-          <Link to={'blog-list'}>Insights</Link>
+        <li className="w-full text-center">
+          <Link to={'blog-list'} className="text-white no-underline">
+            Insights
+          </Link>
         </li>
-        <li>
-          <Link to={'contact'}>Contact</Link>
+        <li className="w-full text-center">
+          <Link to={'contact'} className="text-white no-underline">
+            Contact
+          </Link>
         </li>
       </ul>
     </div>
